90dbf6cf978ac79390303e5ebe3138b3c00a6d02
B00420049-Uzair-Mohammed <b00420049@studentmail.uws.ac.uk>
2025-11-19 10:29:31 +0000

    Village environment
    village environment finished

--- babylonProj/village/src/createStartScene.ts
+++ babylonProj/village/src/createStartScene.ts
@@ -11,253 +11,229 @@ import {
   Color3,
   Engine,
   Texture,
+  CubeTexture,
+  Nullable,
+  Vector4,
+  InstancedMesh,
+  SpriteManager,
+  Sprite
 } from "@babylonjs/core";
 
-//Assets from https://github.com/BabylonJS/Assets/blob/master/Assets.md
-
-function createGround(scene: Scene) {
-  let ground = MeshBuilder.CreateGround(
-    "ground",
-    { width: 3, height: 3 },
-    scene
-  );
-  var groundMaterial = new StandardMaterial("groundMaterial", scene);
-  groundMaterial.backFaceCulling = false;
-  ground.material = groundMaterial;
-  groundMaterial.diffuseColor = new Color3(0.1, 1, 0.5);
-  return ground;
-}
-
-function createSphere(scene: Scene) {
-  let sphere = MeshBuilder.CreateSphere(
-    "sphere",
-    { diameter: 1, segments: 32 },
-    scene
-  );
-  sphere.position.y = 0;
-  var texture = new StandardMaterial("grass1", scene);
-  texture.emissiveTexture = new Texture("./src/assets/textures/lavatile.jpg", scene);
-  sphere.material = texture;
-  return sphere;
-}
-
-function createBox(scene: Scene) {
-  let box = MeshBuilder.CreateBox(
-    "box",
-    { width: 1, height: 1 },
-    scene
-  );
-  box.position.x = -1;
-  box.position.y = 1;
-  box.position.z = 1;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  box.material = texture;
-  return box;
-}
-
-function createCylinder(scene: Scene) {
-  let cylinder = MeshBuilder.CreateCylinder(
-    "cylinder",
-    { height: 1, diameter: 0.7 },
-    scene
-  );
-  cylinder.position.x = 1;
-  cylinder.position.y = 1;
-  cylinder.position.z = 1;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  cylinder.material = texture;
-  return cylinder;
-}
-
-function createCone(scene: Scene) {
-  let cone = MeshBuilder.CreateCylinder(
-    "cone",
-    { height: 1, diameterBottom: 0.7, diameterTop: 0 },
-    scene
-  );
-  cone.position.x = 1;
-  cone.position.y = 1;
-  cone.position.z = -1;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  cone.material = texture;
-  return cone;
-}
-
-function createTriangle(scene: Scene) {
-  let triangle = MeshBuilder.CreateCylinder(
-    "triangle",
-    { height: 1, diameter: 0.7, tessellation: 3 },
-    scene
-  );
-  triangle.position.x = -1;
-  triangle.position.y = 1;
-  triangle.position.z = -1;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  triangle.material = texture;
-  return triangle;
-}
-
-function createCapsule(scene: Scene) {
-  let capsule = MeshBuilder.CreateCapsule(
-    "capsule",
-    { radius: 0.35, height: 1, radiusTop: 0.1 },
-    scene
-  );
-  capsule.position.x = -1;
-  capsule.position.y = -1;
-  capsule.position.z = -1;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 0.6, 0.6);
-  capsule.material = texture;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  capsule.material = texture;
-  return capsule;
-}
-function createTorus(scene: Scene) {
-  let torus = MeshBuilder.CreateTorus(
-    "torus",
-    { diameter: 0.7, thickness: 0.6, tessellation: 10 },
-    scene
-  );
-  torus.position.x = -1;
-  torus.position.y = -1;
-  torus.position.z = 1;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(0.6, 0.6, 1);
-  torus.material = texture;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  torus.material = texture;
-  return torus;
-}
-function createTube(scene: Scene) {
-  const myPath = [
-    new Vector3(0.85, -0.85, 0.85),
-    new Vector3(0.35, -0.35, 0.35),
-  ];
-
-  const tube = MeshBuilder.CreateTube(
-    "tube",
-    { path: myPath, radius: 0.4, sideOrientation: Mesh.DOUBLESIDE },
-    scene
+function createTerrain(scene: Scene) {
+  //Create large ground for valley environment
+  const largeGroundMat = new StandardMaterial("largeGroundMat");
+  largeGroundMat.diffuseTexture = new Texture(
+    "./assets/environments/valleygrass.png"
   );
 
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  tube.material = texture;
-  return tube;
-}
-function createExtrusion(scene: Scene) {
-  const myShape = [
-    new Vector3(0, 0.7, 0),
-    new Vector3(0.2, 0.2, 0),
-    new Vector3(0.7, 0, 0),
-    new Vector3(0.2, -0.2, 0),
-    new Vector3(0, -0.7, 0),
-    new Vector3(-0.2, -0.2, 0),
-    new Vector3(-0.7, 0, 0),
-    new Vector3(-0.2, 0.2, 0),
-  ];
-
-  const myPath = [
-    new Vector3(0.75, -0.75, -0.2),
-    new Vector3(0.75, -0.75, -1.2),
-  ];
-
-  const extrusion = MeshBuilder.ExtrudeShape(
-    "star",
+  const largeGround = MeshBuilder.CreateGroundFromHeightMap(
+    "largeGround",
+    "./assets/environments/villageheightmap.png",
     {
-      shape: myShape,
-      closeShape: true,
-      path: myPath,
-      sideOrientation: Mesh.DOUBLESIDE,
+      width: 150,
+      height: 150,
+      subdivisions: 20,
+      minHeight: 0,
+      maxHeight: 10,
     },
     scene
   );
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  extrusion.material = texture;
-  return extrusion;
+  largeGround.material = largeGroundMat;
+  largeGround.position.y = -0.01;
 }
-function createOctahedron(scene: Scene) {
-  let octahedron = MeshBuilder.CreatePolyhedron(
-    "oct",
-    { type: 1, size: 0.35 },
+
+function createGround(scene: Scene) {
+  const groundMaterial = new StandardMaterial("groundMaterial");
+  groundMaterial.diffuseTexture = new Texture(
+    "./assets/environments/villagegreen.png"
+  );
+  groundMaterial.diffuseTexture.hasAlpha = true;
+  groundMaterial.backFaceCulling = false;
+  const ground = MeshBuilder.CreateGround(
+    "ground",
+    { width: 24, height: 24 },
     scene
   );
-  octahedron.position.x = 0;
-  octahedron.position.y = 2.5;
-  octahedron.position.z = 0;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/reflectivity.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  octahedron.material = texture;
-  return octahedron;
+  ground.material = groundMaterial;
+  ground.position.y = 0.01;
+  return ground;
 }
-function createPlane(scene: Scene) {
-  let plane = MeshBuilder.CreatePlane(
-    "plane",
-    { size: 3, sideOrientation: Mesh.DOUBLESIDE },
+
+function createSky(scene: Scene) {
+  const skybox = MeshBuilder.CreateBox("skyBox", { size: 150 }, scene);
+  const skyboxMaterial = new StandardMaterial("skyBox", scene);
+  skyboxMaterial.backFaceCulling = false;
+  skyboxMaterial.reflectionTexture = new CubeTexture(
+    "./assets/textures/skybox/skybox",
     scene
   );
-  plane.position.y = 0;
-
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/wood.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  plane.material = texture;
-  return plane;
+  skyboxMaterial.reflectionTexture.coordinatesMode =
+    Texture.SKYBOX_MODE;
+  skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
+  skyboxMaterial.specularColor = new Color3(0, 0, 0);
+  skybox.material = skyboxMaterial;
+  return skybox;
 }
 
-function createPlane2(scene: Scene) {
-  let plane = MeshBuilder.CreatePlane(
-    "plane",
-    { size: 3, sideOrientation: Mesh.DOUBLESIDE },
+function createBox(style: number) {
+  //style 1 small style 2 semi detatched
+  const boxMat = new StandardMaterial("boxMat");
+  const faceUV: Vector4[] = []; // faces for small house
+  if (style == 1) {
+    boxMat.diffuseTexture = new Texture("./assets/textures/cubehouse.png");
+    faceUV[0] = new Vector4(0.5, 0.0, 0.75, 1.0); //rear face
+    faceUV[1] = new Vector4(0.0, 0.0, 0.25, 1.0); //front face
+    faceUV[2] = new Vector4(0.25, 0, 0.5, 1.0); //right side
+    faceUV[3] = new Vector4(0.75, 0, 1.0, 1.0); //left side
+    // faceUV[4] would be for bottom but not used
+    // faceUV[5] would be for top but not used
+  } else {
+    boxMat.diffuseTexture = new Texture("./assets/textures/semihouse.png");
+    faceUV[0] = new Vector4(0.6, 0.0, 1.0, 1.0); //rear face
+    faceUV[1] = new Vector4(0.0, 0.0, 0.4, 1.0); //front face
+    faceUV[2] = new Vector4(0.4, 0, 0.6, 1.0); //right side
+    faceUV[3] = new Vector4(0.4, 0, 0.6, 1.0); //left side
+    // faceUV[4] would be for bottom but not used
+    // faceUV[5] would be for top but not used
+  }
+  
+  const box = MeshBuilder.CreateBox("box", {
+    width: style,
+    height: 1,
+    faceUV: faceUV,
+    wrap: true,
+  });
+  box.position = new Vector3(0, 0.5, 0);
+  box.scaling = new Vector3(1, 1, 1);
+  box.material = boxMat;
+  return box;
+}
+
+function createRoof(style: number) {
+  const roof = MeshBuilder.CreateCylinder("roof", {
+    diameter: 1.3,
+    height: 1.2,
+    tessellation: 3,
+  });
+  roof.scaling.x = 0.75;
+  roof.scaling.y = style * 0.85;
+  roof.rotation.z = Math.PI / 2;
+  roof.position.y = 1.22;
+  const roofMat = new StandardMaterial("roofMat");
+  roofMat.diffuseTexture = new Texture("./assets/textures/roof.jpg");
+  roof.material = roofMat;
+  return roof;
+}
+
+function createHouse(scene: Scene, style: number) {
+  const box = createBox(style);
+  const roof = createRoof(style);
+  const house = Mesh.MergeMeshes(
+    [box, roof],
+    true,
+    false,
+    undefined,
+    false,
+    true
+  );
+  // last true allows combined mesh to use multiple materials
+  return house;
+}
+
+function createHouses(scene: Scene, style: number) {
+  //Start by locating one each of the two house types then add others
+
+  if (style == 1) {
+    // show 1 small house
+    createHouse(scene, 1);
+  }
+  if (style == 2) {
+    // show 1 large house
+    createHouse(scene, 2);
+  }
+  if (style == 3) {
+    // show estate
+    const houses: Nullable<Mesh>[] = [];
+    // first two houses are original meshes
+    houses[0] = createHouse(scene, 1);
+    houses[0]!.rotation.y = -Math.PI / 16;
+    houses[0]!.position.x = -6.8;
+    houses[0]!.position.z = 2.5;
+
+    houses[1] = createHouse(scene, 2);
+    houses[1]!.rotation.y = -Math.PI / 16;
+    houses[1]!.position.x = -4.5;
+    houses[1]!.position.z = 3;
+
+    //next houses are cloned instances of first two
+    const ihouses: InstancedMesh[] = [];
+    const places: number[][] = []; //each entry is an array [house type, rotation, x, z]
+
+    places.push([2, -Math.PI / 16, -1.5, 4]);
+    places.push([2, -Math.PI / 3, 1.5, 6]);
+    places.push([2, (15 * Math.PI) / 16, -6.4, -1.5]);
+    places.push([1, (15 * Math.PI) / 16, -4.1, -1]);
+    places.push([2, (15 * Math.PI) / 16, -2.1, -0.5]);
+    places.push([1, (5 * Math.PI) / 4, 0, -1]);
+    places.push([1, Math.PI + Math.PI / 2.5, 0.5, -3]);
+    places.push([2, Math.PI + Math.PI / 2.1, 0.75, -5]);
+    places.push([1, Math.PI + Math.PI / 2.25, 0.75, -7]);
+    places.push([2, Math.PI / 1.9, 4.75, -1]);
+    places.push([1, Math.PI / 1.95, 4.5, -3]);
+    places.push([2, Math.PI / 1.9, 4.75, -5]);
+    places.push([1, Math.PI / 1.9, 4.75, -7]);
+    places.push([2, -Math.PI / 3, 5.25, 2]);
+    places.push([1, -Math.PI / 3, 6, 4]);
+
+    for (let i = 0; i < places.length; i++) {
+      if (places[i][0] === 1) {
+        ihouses[i] = houses[0]!.createInstance("house" + i);
+      } else {
+        ihouses[i] = houses[1]!.createInstance("house" + i);
+      }
+      ihouses[i].rotation.y = places[i][1];
+      ihouses[i].position.x = places[i][2];
+      ihouses[i].position.z = places[i][3];
+    }
+  }
+  // nothing returned by this function
+}
+
+function createTrees(scene: Scene) {
+  const spriteManagerTrees = new SpriteManager(
+    "treesManager",
+    "./assets/sprites/tree.png",
+    2000,
+    { width: 512, height: 1024 },
     scene
   );
-  plane.position.y = 0;
-  plane.rotation = new Vector3(0, Math.PI / 2, 0);
 
-  var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./src/assets/textures/wood.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  plane.material = texture;
-  return plane;
+  //We create trees at random positions
+  for (let i = 0; i < 500; i++) {
+    const tree: Sprite = new Sprite("tree", spriteManagerTrees);
+    tree.position.x = Math.random() * -30;
+    tree.position.z = Math.random() * 20 + 8;
+    tree.position.y = 0.2;
+  }
+
+  for (let i = 0; i < 500; i++) {
+    const tree = new Sprite("tree", spriteManagerTrees);
+    tree.position.x = Math.random() * 25 + 7;
+    tree.position.z = Math.random() * -35 + 8;
+    tree.position.y = 0.2;
+  }
+  // nothing returned by this function
 }
-function createLight(scene: Scene) {
+
+function createHemisphericLight(scene: Scene) {
   const light = new HemisphericLight(
     "light",
-    new Vector3(0, 1, 0),
+    new Vector3(2, 1, 0), // move x pos to direct shadows
     scene
   );
-  light.intensity = 0.7;
-  light.diffuse = new Color3(1, 0.6 , 0.6);
-  light.specular = new Color3(0, 1, 0.4);
+  light.intensity = 0.8;
+  light.diffuse = new Color3(1, 1, 1);
+  light.specular = new Color3(1, 0.8, 0.8);
   light.groundColor = new Color3(0, 0.2, 0.7);
   return light;
 }
@@ -265,7 +241,7 @@ function createLight(scene: Scene) {
 function createArcRotateCamera(scene: Scene) {
   let camAlpha = -Math.PI / 2,
     camBeta = Math.PI / 2.5,
-    camDist = 15,
+    camDist = 25,
     camTarget = new Vector3(0, 0, 0);
   let camera = new ArcRotateCamera(
     "camera1",
@@ -275,45 +251,32 @@ function createArcRotateCamera(scene: Scene) {
     camTarget,
     scene
   );
+  camera.lowerRadiusLimit = 9;
+  camera.upperRadiusLimit = 25;
+  camera.lowerAlphaLimit = 0;
+  camera.upperAlphaLimit = Math.PI * 2;
+  camera.lowerBetaLimit = 0;
+  camera.upperBetaLimit = Math.PI / 2.02;
+
   camera.attachControl(true);
   return camera;
 }
+
 export default function createStartScene(engine: Engine) {
+  let scene   = new Scene(engine);
+  let ground  = createGround(scene);
+  let sky     = createSky(scene);
+  let lightHemispheric = createHemisphericLight(scene);
+  createHouses(scene, 3);
+  createTrees(scene);
+  createTerrain(scene);
+  let camera  = createArcRotateCamera(scene);
 
-  let scene = new Scene(engine);
-  scene.ambientColor = new Color3(1, 1, 1);
-
-  let ground = createGround(scene);
-  let sphere = createSphere(scene);
-  let box = createBox(scene);
-  let cylinder = createCylinder(scene);
-  let cone = createCone(scene);
-  let triangle = createTriangle(scene);
-  let capsule = createCapsule(scene);
-  let torus = createTorus(scene);
-  let plane = createPlane(scene);
-  let tube = createTube(scene);
-  let extrusion = createExtrusion(scene);
-  let octahedron = createOctahedron(scene);
-  let plane2 = createPlane2(scene);
-  let lightHemispheric = createLight(scene);
-  let camera =createArcRotateCamera(scene);
 
   let that: SceneData = {
     scene,
     ground,
-    sphere,
-    box,
-    cylinder,
-    cone,
-    triangle,
-    capsule,
-    torus,
-    plane,
-    tube,
-    extrusion,
-    octahedron,
-    plane2,
+    sky,
     lightHemispheric,
     camera
   };
